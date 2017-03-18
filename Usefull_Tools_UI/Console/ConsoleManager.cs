6da7ea63d115194e3742875792eb8b8cdf228bed
Henri2h <henri.carnot@hotmail.fr>
2017-03-18 01:00:45 +0100

    console view project added

---
+++ Usefull_Tools_UI/Console/ConsoleManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Usefull_Tools_UI.Console
+{
+    public class ConsoleManager
+    {
+        public static List<ConsoleView> consoles = new List<ConsoleView>();
+    }
+}
---
+++ Usefull_Tools_UI/Console/ConsoleView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Usefull_Tools_UI.Console
+{
+    public sealed partial class ConsoleView : UserControl
+    {
+
+        const int maxLenght = 800;
+        public string Text { get; set; }
+        public ConsoleView()
+        {
+            this.InitializeComponent();
+            ConsoleManager.consoles.Add(this);
+        }
+
+        public void WriteLine(string text)
+        {
+            this.Text += text + Environment.NewLine;
+            if (this.Text.Length > maxLenght)
+            {
+                this.Text = this.Text.Substring(this.Text.Length - maxLenght);
+            }
+
+            try
+            {
+
+                ConsoleUItextBox.Text = this.Text;
+                //  ConsoleUITextBoxScroll.ChangeView(ConsoleUITextBoxScroll.ScrollableHeight, 0, ConsoleUITextBoxScroll.ZoomFactor);
+
+            }
+            catch (Exception ex)
+            {
+                ex.Source = "ConsoleView.WriteLine (Console)";
+                ErrorManager.printOut(ex);
+            }
+        }
+        private void ConsoleUIbtReturn_Click(object sender, RoutedEventArgs e)
+        {
+            string comm = ConsoleUIReadBox.Text;
+            Console.setNewLine(comm);
+            ConsoleUIReadBox.Text = "";
+        }
+        public void Clear()
+        {
+            ConsoleUItextBox.Text = "";
+        }
+    }
+}
